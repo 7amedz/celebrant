0c55b0bad79cce7e319e03d4113bb00cd325b58d
Hamed Abdullah <hamed.abdullah.dev@gmail.com>
2026-08-03 02:01:28 +0300

    add signal shutdown

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
+#include <csignal>
 #include <thread>
 
 #include <boost/asio.hpp>
 #include <boost/asio/io_context.hpp>
+#include <boost/asio/signal_set.hpp>
 
 #include "celebrant/connection_registry.hpp"
 #include "celebrant/engine_runner.hpp"
@@ -20,6 +22,12 @@ int main(int argc, char* argv[]) {
 
     std::jthread engine([&runner] { runner.run(); });
     celebrant::Gateway gateway(io, queue, 9001, registry);
+
+    asio::signal_set signals(io, SIGINT, SIGTERM);
+    signals.async_wait([&](const std::error_code&, int) {
+        queue.push(celebrant::Shutdown{}); // shutdown request to engine
+        io.stop();                         // stop io thread
+    });
 
     io.run();
     return 0;
